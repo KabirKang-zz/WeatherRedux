6cd06132b32804960b8227aa7b7df270b67e7f80
Kabir Kang <kangkabir@gmail.com>
2016-12-14 23:03:53 -0800

    added humidity and pressure

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -4,7 +4,6 @@ export default function(state = [], action) {
   console.log('Action received', action);
   switch (action.type) {
     case FETCH_WEATHER:
-    return state.concat([action.payload.data]);
     return [action.payload.data, ...state];
   }
   return state;
